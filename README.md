a4fa9a29742a849eaa21f94d79e59caba5bff3a7
Jerome <jerome_chiu@adata.com>
2022-10-21 16:14:47 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # API_Strsss_Test
 This project is made by python Locust.
+
+
+## Step
+1. Install python in local. Refer to use python3.10
+
+2. Setup virtual environment
+    
+        python3 -m venv env
+
+3. Install all necessary module
+
+        pip install -r requirements.txt
+
+3. Edit your python script
+
+4. Run stress test
+
+        locust -f locustfile.py -H http://{api_server_ip}:{port}
+
+5. Go to Locus Web UI at http://0.0.0.0:8089
---
+++ locustfile.py
@@ -0,0 +1,96 @@
+
+from locust import FastHttpUser, task
+import random
+import json
+import time
+
+class WebsiteUser(FastHttpUser):
+    """
+    User class that does requests to the locust web server running on localhost,
+    using the fast HTTP client
+    """
+
+    host = "http://127.0.0.1:8000"
+    # some things you can configure on FastHttpUser
+    # connection_timeout = 60.0
+    insecure = True
+    # max_redirects = 5
+    max_retries = 1
+    network_timeout = 60.0
+    
+    def on_start(self):
+        """ on_start is called when a Locust start before any task is scheduled """
+        # self.login()
+        pass
+
+    def on_stop(self):
+        """ on_stop is called when the TaskSet is stopping """
+        # self.logout()
+        pass
+
+    def login(self):
+        response = self.client.post("/login", {"username":"ellen_key", "password":"education"})
+        res_data = response.json()
+        '''Get authentication token from response data'''
+
+    def logout(self):
+        self.client.post("/logout", {"username":"ellen_key", "password":"education"})
+
+
+    '''
+    get percetage for each task by using @task decorator
+    '''
+    @task
+    def get_single_Prod(self):
+        pid = 27
+        self.client.get(f"/api/prods?pid={pid}")
+
+    @task(3)
+    def get_all_Prod(self):
+        self.client.get(f"/api/prods")
+    
+    @task(3)
+    def add_a_prod(self):
+        
+        headers = {
+            'content-type': 'application/json',
+            # 'Authorization': 'Basic YWxpY2U6c3VwZXJtYW4='
+            }
+        
+        alphabet = 'abcdefghijklmnopqrstuvwxyz!@#$%^&*()1234567890'
+        subName = random.sample(alphabet, 13)
+        subName = ''.join(subName)
+        padName = f'pr_{subName}'
+        payload = {
+            "name": padName,
+            "amount": 10,
+            "delete_able": False
+        }
+        self.client.post('/api/prods', data=json.dumps(payload), headers=headers)
+        
+    @task(3)
+    def update_a_prod(self):
+        
+        headers = {
+            'content-type': 'application/json',
+            # 'Authorization': 'Basic YWxpY2U6c3VwZXJtYW4='
+            }
+        
+        padName = 'I406'
+        payload = {
+            "name": padName,
+            "amount": 10,
+            "delete_able": False
+        }
+        self.client.put('/api/prods', data=json.dumps(payload), headers=headers)
+        
+    @task(1)
+    def update_a_prod(self):
+        
+        headers = {
+            'content-type': 'application/json',
+            # 'Authorization': 'Basic YWxpY2U6c3VwZXJtYW4='
+            }
+        pid = 27
+        # self.client.delete('/api/prods?pid={pid}', headers=headers)
+    
